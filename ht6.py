dcfd5e3cde4a9014865ad13f6bf60dd806efe231
serhSK <sergeyskotnitskiy@gmail.com>
2017-11-21 02:25:37 +0200

    added simple documenting to Calc class

--- ht6.py
+++ ht6.py
@@ -1,8 +1,13 @@
 class Calc(object):
+    """last_result it is variable where will be saved result of calculation, by default is None"""
     last_result = None
 
+    """these are methods that take 2 arguments a and b and execute corresponding operations with these arguments, 
+    then set the last_result variable to the result of the execution of the operation
+    
+    """
     def add(self, a, b):
-        self.last_result =  a + b
+        self.last_result = a + b
 
     def subtract(self, a, b):
         self.last_result = a - b
